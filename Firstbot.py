1a7f8133f75701f91011bbf807c89c494ee6696e
Vaibhav Tyagi <vaibhavtyagi0808@gmail.com>
2021-05-15 16:16:37 +0530

    My commits

--- Firstbot.py
+++ Firstbot.py
@@ -59,7 +59,7 @@ def main():
                           port=int(PORT),
                           url_path=environ['API_KEY'])
     updater.bot.setWebhook(
-        'https://stock-price-analysis-bot.herokuappname.com/'+environ['API_KEY'])
+        'https://polar-meadow-55829.herokuapp.com/'+environ['API_KEY'])
 
     updater.idle()
 
